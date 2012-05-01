756fda06d0a7cf1f6149433b1d6ed3b26eb756b0
clay woolam <clay@woolam.org>
2012-04-30 17:00:14 -0700

    initial commit

---
+++ src/testing.js
@@ -0,0 +1,12 @@
+exports.testing = exports.testing || {};
+
+exports.testing.fuzzyArrayEqual = function(lhs, rhs, tolerance) {
+  if (!tolerance) { tolerance = 1e-4; }
+  if (lhs.length != rhs.length) { return false; }
+  for (var i = 0; i < lhs.length; i++) {
+    if (Math.abs(lhs[i] - rhs[i]) > tolerance) {
+      return false;
+    }
+  }
+  return true;
+};
